18d65e092d967024f4afe0c779e887d538ce965e
Alto888 <antlvov2023@mail.ru>
2026-04-07 19:47:39 +0300

    Добавление нового питомца

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,5 +1,7 @@
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
 
@@ -29,3 +31,25 @@ class TestPet:
                                                                                                                                                            #В дебаггере посмотрим какой тест должен быть у атрибута "text" и добавим его в тест
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+    @allure.title("Добавление нового питомца")
+    def test_add_pet(self):
+        with allure.step ("Подготовка данных для создания питомца"):
+            payload = {                                                                                                                             #Задаем переменную payload и добавляем туда тело в формате json
+                        "id": 1,
+                        "name": "Buddy",
+                        "status": "available"
+            }
+        with allure.step ("Отправка запроса на создание питомца"):
+            response = requests.post (url=f"{BASE_URL}/pet", json=payload)
+            response_json = response.json()
+        with allure.step ("Проверка статуса ответа и валидация Json схемы"):
+            assert response.status_code == 200
+            jsonschema.validate(response_json, PET_SCHEMA)
+        with allure.step ("Проверка параметров питомца в ответе"):
+            assert response_json ['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
+            assert response_json ['name'] == payload['name'], "name питомца не совпадает с ожидаемым"
+            assert response_json ['status'] == payload['status'], "status питомца не совпадает с ожидаемым"
+
+
+
