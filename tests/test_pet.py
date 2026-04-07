0fecfb3382396692cf0b1dcd179711c4bc9dac8a
Alto888 <antlvov2023@mail.ru>
2026-04-07 20:24:11 +0300

    Добавление нового питомца

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -7,38 +7,26 @@ BASE_URL = "http://5.181.109.28:9090/api/v3"
 
 @allure.feature("Pet")
 class TestPet:
-    @allure.title("Попытка удалить несуществующего питомца")
-    def test_delete_nonexistent_pet(self):
-       with allure.step("Отправка запроса на удаление несуществующего питомца"):
-           response = requests.delete(url=f"{BASE_URL}/pet/999")
-       with allure.step("Проверка статуса ответа"):
-           assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
-       with allure.step("Проверка текстового содержимого ответа"):
-           assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
-
-    @allure.title("Попытка обновить несуществующего питомца")
-    def test_update_nonexistent_pet(self):
-        with allure.step ("Отправка запроса на обновление несуществующего питомца"):
-            payload = {                                                                                                                             #Задаем переменную payload и добавляем туда тело в формате json
-            "id": 9999,
-            "name": "Non-existent Pet",
-            "status": "available"
-        }                                                                                                                                                  #Задаем переменную response с методом put
-            response = requests.put(url=f"{BASE_URL}/pet", json=payload)
-
-        with allure.step("Проверка статуса ответа"):
-            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
-                                                                                                                                                           #В дебаггере посмотрим какой тест должен быть у атрибута "text" и добавим его в тест
-        with allure.step("Проверка текстового содержимого ответа"):
-            assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
-
     @allure.title("Добавление нового питомца")
     def test_add_pet(self):
         with allure.step ("Подготовка данных для создания питомца"):
-            payload = {                                                                                                                             #Задаем переменную payload и добавляем туда тело в формате json
-                        "id": 1,
-                        "name": "Buddy",
-                        "status": "available"
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": [
+                    "string"
+                ],
+                "tags": [
+                    {
+                        "id": 0,
+                        "name": "string"
+                    }
+                ],
+                "status": "available"
             }
         with allure.step ("Отправка запроса на создание питомца"):
             response = requests.post (url=f"{BASE_URL}/pet", json=payload)
